9de352d5e9fef7859042282a7a350f85c05cc9eb
Michael de Villiers <michael@systemworks.co.za>
2019-02-26 11:11:39 +0200

    Revert "update web layout"
    This reverts commit 372922f3b46791bbb0b7c3cd5b388c027a12847f.

--- novasys_custom/__manifest__.py
+++ novasys_custom/__manifest__.py
@@ -1,21 +1,15 @@
 # See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'NOVASYS Customisations',
-    'version': '12.0.1.0.0',
+    'name': 'Novasys Custom',
+    'version': '11.0.1.0.0',
     'category': 'Custom',
     'license': 'AGPL-3',
     'author': 'SystemWorks.',
     'website': 'http://www.systemworks.co.za',
-    'summary': 'Customisations for NOVASYS',
-    'depends': [
-        'base',
-        'web',
-    ],
-    'data': [
-        'views/res_partner_view.xml',
-        'views/web_layout.xml',
-    ],
+    'summary': '',
+    'depends': ['base'],
+    'data': ['views/res_partner_view.xml'],
     'installable': True,
     'auto_install': False
 }
