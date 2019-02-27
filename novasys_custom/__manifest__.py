de0eed7213c5bc1971e22c37e396772b8e7748ed
Michael de Villiers <michael@systemworks.co.za>
2019-02-27 10:23:46 +0200

    updated report layouts

--- novasys_custom/__manifest__.py
+++ novasys_custom/__manifest__.py
@@ -1,15 +1,19 @@
-# See LICENSE file for full copyright and licensing details.
-
 {
-    'name': 'Novasys Custom',
+    'name': 'NOVASYS Customisations',
     'version': '11.0.1.0.0',
     'category': 'Custom',
     'license': 'AGPL-3',
     'author': 'SystemWorks.',
     'website': 'http://www.systemworks.co.za',
-    'summary': '',
-    'depends': ['base'],
-    'data': ['views/res_partner_view.xml'],
+    'summary': 'Customisations for NOVASYS',
+    'depends': [
+        'base',
+        'web',
+    ],
+    'data': [
+        'views/report_layout.xml',
+        'views/res_partner_view.xml',
+    ],
     'installable': True,
     'auto_install': False
 }
